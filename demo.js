bce9e2c67c9d7d1d499f133ce000e387a824c4ef
bnviki <bn.vikram@gmail.com>
2014-01-18 21:11:51 +0530

    experiments initialised

---
+++ demo.js
@@ -0,0 +1,18 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: vikram
+ * Date: 18/1/14
+ * Time: 8:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+window.onload = function(){
+
+    console.log("Hi...") ;
+    var elem=document.createElement("b");
+    var _text = document.createTextNode('Creating Div Element')    ;
+    elem.appendChild(_text);
+    document.body.appendChild(elem)  ;
+
+    //http://mpeers.com/users
+};
